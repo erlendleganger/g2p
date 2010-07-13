becd4adba28fdedef5d8a7da2bc09f29e87ceb13
U-Multicom-680\Erlend <Erlend@Multicom-680.(none)>
2010-07-13 08:31:09 +0200

    Add code to export basic settings

--- bin/g2p.sh
+++ bin/g2p.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+#---------------------------------------------------------------------------
+export BASEDIR=$(cd $(dirname $0);pwd)
+export BASENAME=$(echo $(basename $0)|sed "s/\..*//")
+export CFGFILE=$BASEDIR/$BASENAME.ini
+export PLFILE=$BASEDIR/$BASENAME.pl
+echo BASEDIR=$BASEDIR
+echo CFGFILE=$CFGFILE
+echo BASENAME=$BASENAME
+
+#---------------------------------------------------------------------------
+for F in $CFGFILE $PLFILE; do
+   if [ ! -f $F ]; then
+      echo error - cannot find $F
+   fi
+done
+
+#---------------------------------------------------------------------------
+#parse parameters?
+
+#---------------------------------------------------------------------------
+#run main script
+perl $PLFILE
+
+#---------------------------------------------------------------------------
 echo this is $0
 echo
 echo hit return to exit
