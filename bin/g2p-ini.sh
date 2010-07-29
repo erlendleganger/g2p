06802587ff9ead49af46abfb0fa8d3c830aa9867
Erlend Leganger <erlend.leganger@ssc.nato.int>
2010-07-29 20:57:01 +0200

    Corrected POLARDIR setting

--- bin/g2p-ini.sh
+++ bin/g2p-ini.sh
@@ -5,7 +5,7 @@ aID=(fr310xt e500 tacx)
 
 #---------------------------------------------------------------------------
 aSRCDIR=(
-C:\\Users\\Erlend\\AppData\\Roaming\\GARMIN\\Devices\\3683510935\\History
+C:\\Users\\eleganger\\AppData\\Roaming\\GARMIN\\Devices\\3683510935\\History
 D:\\bruker\\erlend\\personlig\\trening\\garmin\\enhet\\edge500\\Garmin\\Activities
 \\\\sempron3000\\Public\\tacx
 )
@@ -17,8 +17,15 @@ aPATTERN=(*.TCX *.fit *.hrm)
 export POLARDIR="testdata\\Erlend Leganger\\$(date "+%Y")"
 export POLARDIR=/cygdrive/c/tmp
 export POLARDIR="D:\\bruker\\erlend\\personlig\\trening\\polar\\Erlend Leganger\\$(date "+%Y")"
+export POLARDIR="D:\\user\\elega\\personal\\trening\\polar\\Erlend Leganger\\$(date "+%Y")"
 
 #---------------------------------------------------------------------------
+#get_timestamp_filename
+#return the name of the time stamp file for the passed id
+#parameters:
+#- $1: the directory containing the timestamp file
+#- $2: the id for the timestamp file
+#sample call: timestampfile=$(get_timestamp_filename $dir $id)
 #---------------------------------------------------------------------------
 get_timestamp_filename(){
    echo $1/timestamp-$2.txt
