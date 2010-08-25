2bbf07fc5309cbcf41565df8c69372887d82c728
U-Multicom-680\Erlend <Erlend@Multicom-680.(none)>
2010-08-25 19:22:56 +0200

    Updated POLARDIR setting

--- bin/g2p-ini.sh
+++ bin/g2p-ini.sh
@@ -1,11 +1,9 @@
 #---------------------------------------------------------------------------
-TCXDIR=C:\\Users\\Erlend\\AppData\\Roaming\\GARMIN\\Devices\\3683510935\\History
-#---------------------------------------------------------------------------
 aID=(fr310xt e500 tacx)
 
 #---------------------------------------------------------------------------
 aSRCDIR=(
-C:\\Users\\eleganger\\AppData\\Roaming\\GARMIN\\Devices\\3683510935\\History
+C:\\Users\\Erlend\\AppData\\Roaming\\GARMIN\\Devices\\3683510935\\History
 D:\\bruker\\erlend\\personlig\\trening\\garmin\\enhet\\edge500\\Garmin\\Activities
 \\\\sempron3000\\Public\\tacx
 )
@@ -16,8 +14,8 @@ aPATTERN=(*.TCX *.fit *.hrm)
 #---------------------------------------------------------------------------
 export POLARDIR="testdata\\Erlend Leganger\\$(date "+%Y")"
 export POLARDIR=/cygdrive/c/tmp
-export POLARDIR="D:\\bruker\\erlend\\personlig\\trening\\polar\\Erlend Leganger\\$(date "+%Y")"
 export POLARDIR="D:\\user\\elega\\personal\\trening\\polar\\Erlend Leganger\\$(date "+%Y")"
+export POLARDIR="D:\\bruker\\erlend\\personlig\\trening\\polar\\Erlend Leganger\\$(date "+%Y")"
 
 #---------------------------------------------------------------------------
 #get_timestamp_filename
